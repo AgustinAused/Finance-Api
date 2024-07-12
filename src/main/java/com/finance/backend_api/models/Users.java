5e774cea28e902a6ce23208e2457bdd039c72200
Agustin Aused <agus.aused@gmail.com>
2024-07-12 20:09:21 -0300

    feat: Actualiza la tabla  y agrega la entidad  en el modelo
    - Se realizaron cambios en la definición de la tabla  en el archivo  para corregir el esquema de la base de datos.
    - Se creó la entidad  en el modelo de la aplicación () para reflejar la estructura actualizada de la tabla .

--- src/main/java/com/finance/backend_api/models/Users.java
+++ src/main/java/com/finance/backend_api/models/Users.java
@@ -1,2 +1,34 @@
-package com.finance.backend_api.models;public class Users {
+package com.finance.backend_api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID user_id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+
+
 }
