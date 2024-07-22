b89d0450948f0c961c05c434626ee489b28de04f
Agustin Aused <agus.aused@gmail.com>
2024-07-22 20:19:55 -0300

    feat: agregar atributos necesarios para crear usuarios

--- src/main/java/com/finance/backend_api/dtos/UserRequest.java
+++ src/main/java/com/finance/backend_api/dtos/UserRequest.java
@@ -1,6 +1,23 @@
 package com.finance.backend_api.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserRequest {
 
+    private String username;
+    private String password;
+    private String email;
+    private String first_name;
+    private String last_name;
+    private boolean active;
+    private Long company_id;
 
+    public UserRequest() {
+    }
+
+    public UserRequest(String username, String password, String email, String first_name, String last_name, boolean active, Long company_id) {
+    }
 }
