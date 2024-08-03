e1aa72d624d5bbbe6b3cf8aafcd6413bd9381d54
Agustin Aused <agus.aused@gmail.com>
2024-08-03 19:06:39 -0300

    feat: add CategoryRequest DTO for Controller

---
+++ src/main/java/com/finance/backend_api/dtos/CategoryRequest.java
@@ -0,0 +1,24 @@
+package com.finance.backend_api.dtos;
+
+import com.finance.backend_api.services.CategoryService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategoryRequest {
+
+    private Long id;
+
+    private Long companyId;
+    private String name;
+    private String description;
+
+    public CategoryRequest(Long id,String name, String description){
+        this.companyId = id;
+        this.name = name;
+        this.description = description;
+    }
+}
