af1d1912d070cb8965e6da83d7f0245cc18881e2
Agustin Aused <agus.aused@gmail.com>
2025-01-14 20:27:53 -0300

    Add transaction query methods and service logic
    Introduce methods in `TransactionService` for saving and fetching transactions by various criteria such as company, category, and transaction type. Corresponding repository query methods were added to support these operations.

--- src/main/java/com/finance/backend_api/repositories/TransactionRepository.java
+++ src/main/java/com/finance/backend_api/repositories/TransactionRepository.java
@@ -4,8 +4,16 @@ import com.finance.backend_api.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByCompanyId(Long companyId);
+
+    List<Transaction> findByCompanyIdAndTransactionType(Long companyId, String transactionType);
+
+    List<Transaction> findByCategoryIdAndCompany(Long categoryId, Long companyId);
     // custom method
 
 }
--- src/main/java/com/finance/backend_api/services/TransactionService.java
+++ src/main/java/com/finance/backend_api/services/TransactionService.java
@@ -1,9 +1,50 @@
 package com.finance.backend_api.services;
 
+import com.finance.backend_api.models.Transaction;
+import com.finance.backend_api.repositories.TransactionRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public Transaction saveTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+
+    public List<Transaction> getTransactionsByCompanyId(Long companyId) {
+        return transactionRepository.findByCompanyId(companyId);
+    }
+
+    public List<Transaction> getTransactionsByCategoryId(Long categoryId, Long companyId) {
+        return transactionRepository.findByCategoryIdAndCompany(categoryId, companyId);
+    }
+
+    public List<Transaction> getTransactionsByCompanyIdAndUserId(Long companyId) {
+        return transactionRepository.findByCompanyId(companyId);
+    }
+
+    public List<Transaction> getExpensesByCompanyId(Long companyId) {
+        return transactionRepository.findByCompanyIdAndTransactionType(companyId, "expense");
+    }
+
+    public List<Transaction> getIncomesByCompanyId(Long companyId) {
+        return transactionRepository.findByCompanyIdAndTransactionType(companyId, "income");
+    }
+
+
 
 
 }
