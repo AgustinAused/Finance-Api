c8c070d6a8db2f16ede2c725905bfbe6a1373d15
Agustin Aused <agus.aused@gmail.com>
2025-01-14 20:28:42 -0300

    Set category field in Transaction as non-nullable
    This change enforces that the category field in the Transaction model cannot be null by adding the `nullable = false` constraint. It ensures data integrity by requiring all transactions to be associated with a valid category.

--- src/main/java/com/finance/backend_api/models/Transaction.java
+++ src/main/java/com/finance/backend_api/models/Transaction.java
@@ -30,7 +30,7 @@ public class Transaction {
 
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id")
+    @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 
     @Column(name = "description")
