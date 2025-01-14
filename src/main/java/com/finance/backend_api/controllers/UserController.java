c516d2e44bd32a3c84643cca806da15b7be089ad
Agustin Aused <agus.aused@gmail.com>
2025-01-14 20:12:32 -0300

    Fix base URL in UserController annotation
    Updated the `@RestController` annotation to use `/api/user` as the base URL instead of `/user`. This ensures proper routing alignment with the API structure.

--- src/main/java/com/finance/backend_api/controllers/UserController.java
+++ src/main/java/com/finance/backend_api/controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController("/user")
+@RestController("/api/user")
 public class UserController {
 
     private final UserService service;
