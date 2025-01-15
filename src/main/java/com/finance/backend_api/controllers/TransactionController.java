80c66abbbe2d8aedcf7ccf24f1058eda36972249
Agustin Aused <agus.aused@gmail.com>
2025-01-14 21:06:38 -0300

    Add TransactionController and TransactionRequest classes
    Introduce TransactionController to manage transaction operations, including adding and deleting transactions via API endpoints. Add TransactionRequest DTO to handle incoming transaction data with support for optional fields like receipt URL and description.

---
+++ src/main/java/com/finance/backend_api/controllers/TransactionController.java
@@ -0,0 +1,45 @@
+package com.finance.backend_api.controllers;
+
+
+import com.finance.backend_api.dtos.TransactionRequest;
+import com.finance.backend_api.models.Transaction;
+import com.finance.backend_api.services.TransactionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/transactions")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addTransaction(@RequestBody TransactionRequest transactionRequest) {
+        try {
+            Transaction transaction = transactionService.saveTransaction(transactionRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body(transaction);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<?> deleteTransaction(@RequestParam Long transactionId) {
+        try {
+            Transaction transaction = transactionService.deleteTransaction(transactionId);
+            return ResponseEntity.status(HttpStatus.OK).body(transaction);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+
+
+}
---
+++ src/main/java/com/finance/backend_api/dtos/TransactionRequest.java
@@ -0,0 +1,42 @@
+package com.finance.backend_api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TransactionRequest {
+
+    private Long company_id;
+    private Long category_id;
+    private Long user_id;
+    private String transaction_type;
+    private double amount;
+    private Date date;
+
+//    optional
+    private String receipt_url;
+    private String description;
+
+
+    public TransactionRequest() {
+    }
+
+    public TransactionRequest(Long company_id, Long category_id, Long user_id, String transaction_type, double amount, Date date, Optional<String> receipt_url, Optional<String> description) {
+        this.company_id = company_id;
+        this.category_id = category_id;
+        this.user_id = user_id;
+        this.transaction_type = transaction_type;
+        this.amount = amount;
+        this.date = date;
+        this.receipt_url = receipt_url.orElse(null);
+        this.description = description.orElse(null);
+    }
+
+
+}
