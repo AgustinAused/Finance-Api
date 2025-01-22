d2c85fe89f5ecaa6a0630c28a0859d3ce336b9e4
Agustin Aused <agus.aused@gmail.com>
2025-01-21 22:20:41 -0300

    Add Constructor

--- src/main/java/com/finance/backend_api/request/ChangePasswordRequest.java
+++ src/main/java/com/finance/backend_api/request/ChangePasswordRequest.java
@@ -1,15 +1,21 @@
 package com.finance.backend_api.request;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChangePasswordRequest {
 
     private String email;
     private String newPassword;
+    private String oldPassword;
+
+    public ChangePasswordRequest(String email, String newPassword){
+        this.email = email;
+        this.newPassword = newPassword;
+    }
 
 }
