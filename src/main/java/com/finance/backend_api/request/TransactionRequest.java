5b0d6aa677c06e3a5d28c5641765d257ff2112b5
Agustin Aused <agus.aused@gmail.com>
2025-01-26 19:05:31 -0300

    Add a No arguments Contructor

--- src/main/java/com/finance/backend_api/request/TransactionRequest.java
+++ src/main/java/com/finance/backend_api/request/TransactionRequest.java
@@ -3,13 +3,9 @@ package com.finance.backend_api.request;
 import lombok.*;
 
 import java.util.Date;
-import java.util.Optional;
 
 @Getter
 @Setter
-@AllArgsConstructor
-//@RequiredArgsConstructor
-@NoArgsConstructor
 public class TransactionRequest {
 
     private Long company_id;
@@ -24,14 +20,18 @@ public class TransactionRequest {
     private String description;
     private Long id;
 
+    public TransactionRequest(){
 
-    public TransactionRequest(Long company_id, Long category_id, Long user_id, double amount, Date date, String transaction_type, String receipt_url, String description) {
+    }
+
+    public TransactionRequest(Long company_id, Long category_id, Long user_id, double amount, Date date, String transaction_type, String description ) {
         this.company_id = company_id;
         this.category_id = category_id;
         this.user_id = user_id;
         this.amount = amount;
         this.date = date;
         this.transaction_type = transaction_type;
+        this.description = description;
     }
 
 
